2e575993490574e96d63f25bb0b75f932a9e0033
pickforc <cpickford@ca.ibm.com>
2017-01-05 15:52:30 -0500

    Updated for Diego

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.set('view engine', 'jade');
 app.get('/', function(req, res){
 	res.render('index');
 });
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-var port = (process.env.VCAP_APP_PORT || 3000);
-app.listen(port, host);
+var port = (process.env.PORT || 3000);
+app.listen(port);
 console.log('App started on port ' + port);
